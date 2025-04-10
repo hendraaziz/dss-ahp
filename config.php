384c722d379e92faff1f49375f1963e2f4c424ad
hendraaziz <hendraaziz80@gmail.com>
2025-04-10 14:04:00 +0700

    updated3

--- config.php
+++ config.php
@@ -4,12 +4,4 @@ $config["server"] = 'www.db4free.net';
 $config["username"] = 'dss_ahp';
 $config["password"] = 'tugasdss';
 $config["database_name"] = 'dss_ahp';
-#$config["server"] = 'mysql-dssahp-dss-ahp.b.aivencloud.com';
-#$config["username"] = 'avnadmin';
-#$config["password"] = '';
-#$config["database_name"] = 'defaultdb';
-#$config["port"] = 27151;
-#$config["ssl"] = array(
-#    "ca" => __DIR__ . '/ca.pem',
-#    "verify_server_cert" => true
-#);
+
